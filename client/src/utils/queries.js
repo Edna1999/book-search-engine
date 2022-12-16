ef8d163f653400c0181ff6b0907598460b5ddcaf
Edna Omadjambe <ednaomadjambe@Ednas-Air.attlocal.net>
2022-12-16 13:39:34 -0500

    mutation and query files for client

---
+++ client/src/utils/queries.js
@@ -0,0 +1,65 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      books {
+        _id
+        description
+        title
+        author
+        image
+        link
+    
+      }
+    }
+  }
+`;
+
+export const QUERY_BOOKS = gql`
+  query getBooks {
+    books {
+        _id
+        description
+        title
+        author
+        image
+        link
+    }
+  }
+`;
+
+export const QUERY_SINGLE_BOOK = gql`
+  query getSingleBook($bookId: ID!) {
+    book(bookId: $bookId) {
+        _id
+        description
+        title
+        author
+        image
+        link
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      books {
+        _id
+        description
+        title
+        author
+        image
+        link
+    }
+    }
+  }
+`;
+
