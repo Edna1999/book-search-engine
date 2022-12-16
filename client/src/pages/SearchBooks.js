5f28a6a05e0302789e414fe1555483af30d03144
Edna Omadjambe <ednaomadjambe@Ednas-Air.attlocal.net>
2022-12-16 13:59:56 -0500

    used useMutation hook to save book in searchbook.js file

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
+import { useMutation, useQuery } from '@apollo/client';
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { SAVE_BOOK } from '../utils/mutations';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -20,6 +21,8 @@ const SearchBooks = () => {
     return () => saveBookIds(savedBookIds);
   });
 
+
+  const [saveBook, { error }] = useMutation(SAVE_BOOK)
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -65,7 +68,11 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({
+        variables: { ...bookData }
+      })
+    
+    
 
       if (!response.ok) {
         throw new Error('something went wrong!');
